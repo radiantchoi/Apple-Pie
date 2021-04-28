9c70f950354952b15876f1b4b7c2e87a3cf70b12
Gordon Choi <thefirate@gmail.com>
2021-04-28 18:48:20 +0900

    Arrange file paths and codes within extension

--- Apple Pie/ViewControllers/ViewController.swift
+++ Apple Pie/ViewControllers/ViewController.swift
@@ -9,24 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var treeImageView: UIImageView!
-    @IBOutlet var correctWordLabel: UILabel!
-    @IBOutlet var scoreLabel: UILabel!
-    @IBOutlet var letterButtons: [UIButton]!
+    @IBOutlet private var treeImageView: UIImageView! // IBOutlet 변수
+    @IBOutlet private var correctWordLabel: UILabel!
+    @IBOutlet private var scoreLabel: UILabel!
+    @IBOutlet private var letterButtons: [UIButton]!
 
-    var listOfWords = ["buccaneer", "swift", "glorious", "incandescent", "bug", "program"]
-    let incorrectMovesAllowed = 7
-    var totalWins = 0 {
+    private var listOfWords = ["buccaneer", "swift", "glorious", "incandescent", "bug", "program"] // 일반 변수 및 상수
+    private let incorrectMovesAllowed = 7
+    private var totalWins = 0 {
         didSet { newRound() }
     }
-    var totalLosses = 0 {
+    private var totalLosses = 0 {
         didSet { newRound() }
     }
-    var currentGame: Game!
+    private var currentGame: Game!
+}
     
+extension ViewController { // view 관련 코드
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        newRound()
+    }
+}
 
+extension ViewController { // 라이프사이클 관련 코드
+    private func updateUI() {
+        var letters = [String]()
+        for letter in currentGame.formattedWord {
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctWordLabel.text = wordWithSpacing
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMoveRemaining)")
+    }
     
-    func newRound() {
+    private func enableLetterButtons(_ enable: Bool) {
+        for button in letterButtons {
+            button.isEnabled = enable
+        }
+    }
+}
+    
+    
+extension ViewController { // 로직 관련 코드
+    private func newRound() {
         let num = Int.random(in: 0...listOfWords.count-1)
         if !listOfWords.isEmpty {
             let newWord = listOfWords.remove(at: num)
@@ -38,13 +65,7 @@ class ViewController: UIViewController {
         }
     }
     
-    func enableLetterButtons(_ enable: Bool) {
-        for button in letterButtons {
-            button.isEnabled = enable
-        }
-    }
-    
-    func updateGameState() {
+    private func updateGameState() {
         if currentGame.incorrectMoveRemaining == 0 {
             totalLosses += 1
         } else if currentGame.word == currentGame.formattedWord {
@@ -53,28 +74,10 @@ class ViewController: UIViewController {
             updateUI()
         }
     }
+}
     
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        newRound()
-    }
-    
-    func updateUI() {
-        var letters = [String]()
-        for letter in currentGame.formattedWord {
-            letters.append(String(letter))
-        }
-        let wordWithSpacing = letters.joined(separator: " ")
-        correctWordLabel.text = wordWithSpacing
-        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
-        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMoveRemaining)")
-    }
-    
-    
-    
-    @IBAction func buttonPressed(_ sender: UIButton) {
+extension ViewController { // IBAction 코드
+    @IBAction private func buttonPressed(_ sender: UIButton) {
         sender.isEnabled = false
         let letterString = sender.title(for: .normal)!
         let letter = Character(letterString.lowercased())
